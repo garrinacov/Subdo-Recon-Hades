9996c50618928ecf87daf5682de79ab6333d9d14
Aura Hades <39928849+garrinacov@users.noreply.github.com>
2025-03-18 13:19:25 +0700

    Update subdomain_discovery.sh
    remove amass and change findomain
    remove gobuster and change dnsx
    add httpx

--- subdomain_discovery.sh
+++ subdomain_discovery.sh
@@ -54,10 +54,12 @@ check_install() {
 # Install tools if not installed
 check_install "subfinder" || { go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest && mv subfinder /usr/local/bin; }
 check_install "assetfinder" || { go install github.com/tomnomnom/assetfinder@latest && mv assetfinder /usr/local/bin; }
-check_install "amass" || { go install github.com/owasp-amass/amass/v4/...@master && mv amass /usr/local/bin; }
 check_install "sublist3r" || { git clone https://github.com/aboul3la/Sublist3r.git && cd Sublist3r && pip3 install; }
-check_install "gobuster" || { go install github.com/OJ/gobuster/v3@latest && mv gobuster /usr/local/bin; }
+check_install "findomain" || { git clone https://github.com/findomain/findomain.git && cd findomain && apt update && apt install -y cargo && cargo build --release && sudo mv target/release/findomain /usr/local/bin/ && cd .. && rm -rf findomain; }
 check_install "anew" ||  { go install -v github.com/tomnomnom/anew@latest && mv anew /usr/local/bin;  }
+check_install "dnsx" || { go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest && mv dnsx /usr/local/bin; }
+check_install "httpx" || { go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest && mv httpx /usr/local/bin; }
+
 # Begin subdomain discovery process
 echo -e "\e[1;36mRunning Discovery Tools for $file...\e[0m"
 
@@ -66,12 +68,13 @@ subfinder -d $file -all -o subfinder.txt
 assetfinder -subs-only $file | anew assetfinder.txt
 sublist3r -d $file -v -o sublister.txt
 curl -s "https://crt.sh/?q=$file" | grep -oP "(?<=<TD>)[a-zA-Z0-9.-]+\\.$file(?=</TD>)" | sort -u | anew crt.txt
-amass enum -d $file -brute $wordlist | anew amass.txt
-gobuster dns -d $file -w $wordlist -q -t 30 --wildcard | awk '{print $2}' > gobuster.txt
-
+findomain --quiet -t $file | anew findomain.txt
+cat subfinder.txt assetfinder.txt sublister.txt crt.txt findomain.txt | sort -u | dnsx -silent -o domain.txt
+cat domain.txt | httpx -o live-domain.txt 
+rm subfinder.txt assetfinder.txt sublister.txt crt.txt findomain.txt domain.txt
 
 # Final message
 echo -e "\e[1;32mSubdomain Discovery Completed!\e[0m"
 echo -e "\e[1;33mResults have been saved in the following files:\e[0m"
-echo -e "\e[1;32msubfinder.txt, assetfinder.txt, sublister.txt, crt.txt, amass.txt, gobuster.txt\e[0m"
+echo -e "\e[1;32msubfinder.txt, assetfinder.txt, sublister.txt, crt.txt, findomain.txt\e[0m"
 echo "========================================================================="
